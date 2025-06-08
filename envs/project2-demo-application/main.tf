078c0d729e6021d7657d5209fc08040d778abc61
SANKET\ADMIN <sanketsp60@gmail.com>
2025-06-09 01:13:28 +0530

    project updated

--- envs/project2-demo-application/main.tf
+++ envs/project2-demo-application/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraform-s3-backend-bucket-project"
-    key            = "demo-application-statefile/terraform.tfstate"    
+    key            = "demo-application-statefile/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-state-lock"
     encrypt        = true
