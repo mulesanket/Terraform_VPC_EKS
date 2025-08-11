59511cc3f759698e706936a6dd2fe94af37b626a
Sanket Arun Mule <sanketmu@cybage.com>
2025-08-11 16:41:30 +0530

    updated backend

--- backend/main.tf
+++ backend/main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "s3_backend_bucket" {
-  bucket = "terraform-s3-backend-bucket-project"
+  bucket = "myeks-demo-terraform-state"
 
   lifecycle {
     prevent_destroy = false
@@ -7,7 +7,7 @@ resource "aws_s3_bucket" "s3_backend_bucket" {
 }
 
 resource "aws_dynamodb_table" "state_lock_table" {
-  name         = "terraform-state-lock"
+  name         = "myeks-demo-terraform-state-lock"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -66,9 +66,9 @@ resource "aws_eks_node_group" "eks_nodes" {
   subnet_ids      = var.subnet_ids
 
   scaling_config {
-    desired_size = 2
-    max_size     = 3
-    min_size     = 1
+    desired_size = 3
+    max_size     = 5
+    min_size     = 2
   }
 
   instance_types = [var.instance_type]
